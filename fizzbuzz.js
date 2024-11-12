03ca54a1daeae8cbe1f68255cbd81306e24e7485
ahmedobeidi <alobeidi.dev@gmail.com>
2024-11-12 13:27:19 +0100

    ADD: en 2 if

---
+++ fizzbuzz.js
@@ -0,0 +1,54 @@
+/* en 4 if
+let number = 10;
+
+if ((number % 3 == 0) && (number % 5 == 0)) {
+    console.log("fizzbuzz");
+} 
+else if ((number % 3 == 0)) {
+    console.log("fizz");
+}
+else if ((number % 5 == 0)) {
+    console.log("buzz");
+}
+else {
+    console.log(number);
+}
+*/
+
+/* en 3 if
+let nombre = 30;
+
+let resultat ="le resultat est ";
+
+if (nombre % 3 === 0){
+    resultat = resultat + "fizz";
+}
+
+if (nombre % 5 === 0){
+    resultat = resultat + "buzz";
+}
+
+if (nombre % 5 != 0 && nombre % 3 != 0) {
+    resultat = resultat + nombre
+}
+
+console.log(resultat);
+*/
+
+/* en 2 if 
+let nombre = 30;
+
+let printable = "";
+
+if (nombre % 3 === 0) {
+  printable += "fizz";
+}
+
+if (nombre % 5 === 0) {
+  printable += "buzz";
+}
+
+console.log(printable || nombre);
+
+console.log(resultat);
+*/
